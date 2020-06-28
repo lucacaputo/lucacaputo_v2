53b2f814bc808a654aac8a624af0b82dbdc8e52c
Luca Caputo <luca.caputo4@studenti.unimi.it>
2020-06-28 18:53:24 +0200

    typewriter done TODO: styling

---
+++ lucacaputo/components/TypeWriter/TypeWriter.tsx
@@ -0,0 +1,101 @@
+import { useEffect, useState } from "react";
+
+interface TypeWriterProps {
+    wordSequence: Array<string>;
+    beforeSentence: string;
+    afterSentence: string;
+    timing?: number;
+    textColor?: string;
+    bg?: string;
+    fontSize?: number;
+}
+
+const getIntervalledLetters = async (letters: Array<string>, cb: (lett: string) => void, time: number) => {
+    return await new Promise((res, rej) => {
+        let [first, ...rest] = letters;
+        if (rest.length === 0) {
+            cb(first);
+            setTimeout(res, time*5);
+        }
+        else {
+            cb(first);
+            setTimeout(() => {
+                res(getIntervalledLetters(rest, cb, time));
+            }, time)
+        }
+    })
+    .then(() => letters)
+    .catch(err => console.log(err));
+}
+
+const TypeWrriter: React.FC<TypeWriterProps> = ({ wordSequence, beforeSentence, afterSentence, textColor, bg, fontSize, timing=400 }) => {
+    const splittedWords = wordSequence.map(el => el.trim().split(""));
+    const [content, setContent] = useState<Array<string>>([]);
+    const [index, setIndex] = useState(0);
+    useEffect(() => {
+        getIntervalledLetters(splittedWords[index], l => setContent(c => [...c,l]), timing)
+        .then(arr => getIntervalledLetters(arr as Array<string>, l => setContent(c => c.slice(0, c.length-1)), 100))
+        .then(() => {
+            if (index === splittedWords.length-1) setIndex(0);
+            else setIndex(index+1);
+        })
+    }, [index]);
+    return (
+        <div className="typeWriter">
+            <span>{beforeSentence}</span>&nbsp;
+            <span className="animatableSpan">
+                {content.join("")}
+                <span className="cursor" />
+            </span>&nbsp;
+            <span style={{marginLeft: 7}}>{afterSentence}</span>
+
+            <style jsx>
+                {`
+                    .typeWriter {
+                        display: flex;
+                        position: relative;
+                        width: 100%;
+                        flex-wrap: wrap;
+                        background-color: ${bg ?? "#141414"};
+                    }
+                    span {
+                        position: relative;
+                        color: ${textColor ?? "#fff"};
+                        font-size: ${fontSize ?? 16}px;
+                        line-height: ${fontSize ? fontSize+4 : 20}px;
+                        font-family: "Roboto Mono";
+                    }
+                    .cursor {
+                        position: absolute;
+                        transform: translateX(100%);
+                        right: 0;
+                        top: 0;
+                        display: block;
+                        height: 100%;
+                        width: 7px;
+                        background-color: ${textColor ?? "#fff"};
+                        animation-name: blink;
+                        animation-timing-function: linear;
+                        animation-iteration-count: infinite;
+                        animation-duration: .5s;
+                        will-change: opacity;
+                    }
+                    @keyframes blink {
+                        0% {
+                            opacity: 0;
+                        }
+                        50% {
+                            opacity: 1;
+                        }
+                        100% {
+                            opacity: 0;
+                        }
+                    }
+                `}
+            </style>
+
+        </div>
+    );
+}
+
+export default TypeWrriter;
--- lucacaputo/pages/index.tsx
+++ lucacaputo/pages/index.tsx
@@ -3,6 +3,7 @@ import CanvasContext from "../contextes/CanvasContext";
 import ScrollWheel from "../components/ScrollWheel";
 import Terminal from "../components/Terminal/Terminal";
 import { useRef } from "react";
+import TypeWrriter from "../components/TypeWriter/TypeWriter";
 
 const Home = () => {
     const scrollHere = useRef<null | HTMLDivElement>(null);
@@ -18,6 +19,11 @@ const Home = () => {
             </CanvasContext>
             <div className="mainContainer" ref={scrollHere}>
                 <Terminal />
+                <TypeWrriter 
+                    wordSequence={["ciao","sono","luca"]}
+                    beforeSentence="una frase di questa lunghezza"
+                    afterSentence="un'altra frase"
+                />
             </div>
         </>
     );
