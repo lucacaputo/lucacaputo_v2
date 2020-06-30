18aed842382b02610d11481aa198b70fc18e5671
Luca Caputo <caputo.luca@outlook.com>
2020-06-30 14:59:50 +0200

    gesture??

--- lucacaputo/components/SwipeChange/SwipeChange.tsx
+++ lucacaputo/components/SwipeChange/SwipeChange.tsx
@@ -1,11 +1,12 @@
 import { animated, useSpring } from "react-spring";
-import { useDrag } from "react-use-gesture";
+import { useGesture } from "react-use-gesture";
 import styled from "styled-components";
+import useMeasure from "react-use-measure";
 import { useRef } from "react";
+import clamp from "lodash.clamp";
 
 const SwipeContainer = styled(animated.div)`
     position: absolute;
-    z-index:2;
     top: 0;
     left: 0;
     height: 100%;
@@ -13,21 +14,21 @@ const SwipeContainer = styled(animated.div)`
     background-color: yellow;
 `;
 
-const toPerc = (tot: number, movement: number) => {
-    return Math.floor((Math.abs(movement)*100)/tot);
-}
-
 const SwipeChange: React.FC = () => {
-    const contRef = useRef<null | HTMLDivElement>(null);
+    const ball = useRef<null | HTMLDivElement>(null);
+    const [ref, b] = useMeasure();
     const [{ x }, set] = useSpring(() => ({ x: 0 }));
-    const bind = useDrag(({ offset: [x] }) => {
-        set({ x });
-    });
+    const bind = useGesture({
+        onDrag: ({ movement: [mx], memo = x.getValue() }) => {
+            set({ x: clamp(memo + mx, -b.width/2, b.width/2) });
+            return memo;
+        }
+    })
     return (
-        <div className="container" ref={contRef}>
+        <div className="container" ref={ref}>
             <div className="reveal"></div>
             <SwipeContainer style={{ width: x.interpolate(v => `calc(50% + ${v}px)`) }}>
-                <div className="dragBall" {...bind()} />
+                <div className="dragBall" onClick={e => e.stopPropagation()} ref={ball} {...bind()} />
             </SwipeContainer>
             <style jsx>
                 {`
@@ -40,7 +41,7 @@ const SwipeChange: React.FC = () => {
                         width: 100%;
                         height: 500px;
                         background-color: blue;
-                        z-index: 1;
+                        z-index: -1;
                     }
                     .dragBall {
                         width: 50px;
