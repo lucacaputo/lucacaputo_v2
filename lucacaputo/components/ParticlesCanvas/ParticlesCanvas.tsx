61f8c0991ec63158e73946539fcf30f3c4efd2e6
Luca Caputo <luca.caputo4@studenti.unimi.it>
2020-07-30 16:16:08 +0200

    added mouse effect on canvas,, fix performance

--- lucacaputo/components/ParticlesCanvas/ParticlesCanvas.tsx
+++ lucacaputo/components/ParticlesCanvas/ParticlesCanvas.tsx
@@ -7,10 +7,32 @@ import { ParticleBluePrint } from "./Particle";
 interface ParticleCanvasProps {
     proximity_threshold: number;
     partNum: number;
+    mouseArea?: number;
 }
 
-const ParticleCanvas: React.FC<ParticleCanvasProps> = ({ proximity_threshold, partNum }) => {
+const ParticleCanvas: React.FC<ParticleCanvasProps> = ({ proximity_threshold, partNum, mouseArea=240 }) => {
     const ref = useRef<null | HTMLCanvasElement>(null);
+    const mouseCoordinates = useRef({
+        x: 0,
+        y: 0,
+        hovering: false,
+    });
+    const onMouseMove = (evt: React.MouseEvent) => {
+        const { top } = ref.current.getBoundingClientRect();
+        const y = evt.clientY - top;
+        const x = evt.clientX;
+        mouseCoordinates.current = {
+            hovering: true,
+            x,
+            y,
+        }
+    }
+    const onMouseOut = (evt: React.MouseEvent) => {
+        mouseCoordinates.current = {
+            ...mouseCoordinates.current,
+            hovering: false,
+        }
+    }
     useEffect(() => {
         const cnv = ref.current;
         const ctx = cnv.getContext("2d");
@@ -53,9 +75,19 @@ const ParticleCanvas: React.FC<ParticleCanvasProps> = ({ proximity_threshold, pa
                 ))
             }
         }
+        const isInArea = (x: number, y: number, coordinates: { x: number, y: number }, area: number): boolean => {
+            if (x >= coordinates.x - area/2 && x <= coordinates.x + area/2 && y >= coordinates.y - area/2 && y <= coordinates.y + area/2) {
+                return true;
+            }
+            return false;
+        } 
         const animate = () => {
             ctx.clearRect(0, 0, cnv.width, cnv.height);
             let proximity: Array<Array<ParticleBluePrint>> = [];
+            let mouseProximity: Array<ParticleBluePrint> = [];
+            if (mouseCoordinates.current.hovering) {
+                mouseProximity = particles.filter(el => isInArea(el.x, el.y, mouseCoordinates.current, mouseArea));
+            }
             particles.forEach((c: ParticleBluePrint, i: number) => {
                 proximity[i] = particles.filter(el => Math.abs(Math.hypot(el.x - c.x, el.y - c.y)) <= proximity_threshold);
                 c.update(cnv.width, cnv.height, ctx);
@@ -75,12 +107,29 @@ const ParticleCanvas: React.FC<ParticleCanvasProps> = ({ proximity_threshold, pa
                     });
                 }
             });
+            if (mouseCoordinates.current.hovering) {
+                ctx.beginPath();
+                ctx.strokeStyle = "red";
+                ctx.lineWidth = .1;
+                mouseProximity.forEach(el => {
+                    ctx.moveTo(el.x + el.size/2, el.y + el.size/2);
+                    mouseProximity.forEach(el2 => {
+                        ctx.lineTo(el2.x + el2.size/2, el2.y + el2.size/2);
+                        ctx.closePath();
+                        ctx.stroke();
+                    })
+                })
+            }
             requestAnimationFrame(animate);
         }
         requestAnimationFrame(animate);
-    }, [ref]);
+    }, [ref, mouseCoordinates]);
     return (
-        <canvas ref={ref}></canvas>
+        <canvas 
+            ref={ref} 
+            onMouseMove={onMouseMove} 
+            onMouseOut={onMouseOut} 
+        />
     );
 }
 
