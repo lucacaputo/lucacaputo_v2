07f100afe22e017a37839af3f5e46aaccd24ba90
Luca Caputo <luca.caputo4@studenti.unimi.it>
2020-08-02 20:56:29 +0200

    removed sperimental components

--- lucacaputo/pages/index.tsx
+++ lucacaputo/pages/index.tsx
@@ -2,7 +2,7 @@ import HomeHeader from "../components/HomeHeader";
 import CanvasContext from "../contextes/CanvasContext";
 import ScrollWheel from "../components/ScrollWheel";
 // import Terminal from "../components/Terminal/Terminal";
-import SwipeChange from "../components/SwipeChange/SwipeChange";
+// import SwipeChange from "../components/SwipeChange/SwipeChange";
 import { useRef, useEffect } from "react";
 import { motion } from "framer-motion";
 import ParticlesCanvas from "../components/ParticlesCanvas/ParticlesCanvas";
@@ -29,13 +29,10 @@ const Home = () => {
                     })
                 }} />
                 <div style={{overflowX:"hidden"}} ref={scrollHere}>
-                    <div className="mainContainer">
-                        <SwipeChange />
+                    <div style={{height:"100vh", marginTop: 40}}>
+                        <ParticlesCanvas partNum={isMobile ? 40 : 100} proximity_threshold={isMobile ? 50 : 100} />
                     </div>
                 </div>
-            <div style={{height:"100vh", marginTop: 40}}>
-                <ParticlesCanvas partNum={isMobile ? 40 : 100} proximity_threshold={isMobile ? 50 : 100} />
-            </div>
         </motion.div>
     );
 }
