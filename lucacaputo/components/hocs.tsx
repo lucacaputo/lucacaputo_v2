a0b7844a1e610112b48e8f6cce3186c895a2d0c6
Luca Caputo <luca.caputo4@studenti.unimi.it>
2020-08-26 17:12:53 +0200

    added
     modified withViewport to pass enter and exit functions as props

--- lucacaputo/components/hocs.tsx
+++ lucacaputo/components/hocs.tsx
@@ -1,25 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+interface WithVpProps {
+    onEnter: () => void;
+    onExit?: () => void;
+}
+
 export function withViewport<T extends object>(
             Component: React.ComponentType<T>, 
             onEnterViewport: () => void, 
             onExitViewport?: () => void) {
-    class InViewport extends React.Component<T> {
+    class InViewport extends React.Component<T & WithVpProps> {
         node = null;
         state = {
             isInViewport: false,
         }
         adapt = () => {
             const { isInViewport } = this.state;
+            const { onEnter, onExit } = this.props;
             const wh = window.innerHeight || document.documentElement.clientHeight;
             const { top, bottom } = this.node.getBoundingClientRect();
             if (top < wh && top > 0 && bottom >= 0) {
                 if (!isInViewport) {
-                    this.setState({ isInViewport: true }, onEnterViewport);
+                    this.setState({ isInViewport: true }, onEnter);
                 }
             } else {
-                this.setState({ isInViewport: false }, onExitViewport);
+                if (isInViewport) {
+                    this.setState({ isInViewport: false }, onExit);
+                }
             }
         }
         componentDidMount() {
@@ -37,5 +45,5 @@ export function withViewport<T extends object>(
             );
         }
     }
-    return (props: T) => <InViewport {...props} />
+    return (props: T) => <InViewport onEnter={onEnterViewport} onExit={onExitViewport} {...props} />
 }
