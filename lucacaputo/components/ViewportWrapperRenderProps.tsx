c40b5802f97ea4ff74adb93119ebc329891d9585
Luca Caputo <luca.caputo4@studenti.unimi.it>
2020-08-27 20:24:17 +0200

    added inViewportWrapper with render props pattern

---
+++ lucacaputo/components/ViewportWrapperRenderProps.tsx
@@ -0,0 +1,44 @@
+import { useRef, useEffect, useState } from "react";
+import { isInViewport } from "./Helpers";
+
+interface WrapperProps {
+    children: <T>(ref: React.RefObject<T>) => React.ReactNode;
+    onEnterViewport: () => void;
+    onExitViewport?: () => void;
+}
+
+const ViewportWrapper: React.FC<WrapperProps> = ({
+    onEnterViewport,
+    onExitViewport = () => null,
+    children,
+}) => {
+    const ref = useRef<HTMLElement>();
+    const [inViewport, setInViewport] = useState(false);
+    useEffect(() => {
+        const trigger = () => {
+            if (isInViewport(ref.current)) {
+                if (!inViewport) {
+                    setInViewport(true);
+                    onEnterViewport();
+                }
+            } else {
+                if (inViewport) {
+                    setInViewport(false);
+                    onExitViewport();
+                }
+            }
+        }
+        trigger();
+        window.addEventListener("scroll", trigger);
+        return () => {
+            window.removeEventListener("scroll", trigger);
+        }
+    }, [inViewport, ref]);
+    return (
+        <>
+            { children(ref) }
+        </>
+    );
+}
+
+export default ViewportWrapper;
